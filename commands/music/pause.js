9bbc13226eb3ab5006981c0d90d4f147bb4023b0
6NCLegend9 <59505853+6NCLegend9@users.noreply.github.com>
2020-07-19 22:38:42 +0300

    Update pause.js

--- commands/music/pause.js
+++ commands/music/pause.js
@@ -28,7 +28,7 @@ module.exports = class PauseCommand extends Command {
     message.guild.musicData.songDispatcher.pause();
       const embed = new MessageEmbed()
       .setColor(0x00AE86)
-    	.addField('Pause', `Song paused :pause_button: `)
+    	.addField('Pause', `Song paused <:green_pause:734414311262388234> `)
       //.addField('Pause', `Song paused <:pause:713742415080718337>`)
     	return message.embed(embed);
   }
