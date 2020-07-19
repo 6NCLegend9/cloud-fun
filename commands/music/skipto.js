5e480fdaf7df7892eb06a10a700384d268908f08
6NCLegend9 <59505853+6NCLegend9@users.noreply.github.com>
2020-07-19 22:24:20 +0300

    Update skipto.js

--- commands/music/skipto.js
+++ commands/music/skipto.js
@@ -1,4 +1,5 @@
 const { Command } = require('discord.js-commando');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = class SkipToCommand extends Command {
   constructor(client) {
