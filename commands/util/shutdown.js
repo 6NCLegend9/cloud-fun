9e6020a8ed28955848871c30c4f6e31d330cf7f9
6NCLegend9 <59505853+6NCLegend9@users.noreply.github.com>
2020-07-19 22:09:00 +0300

    Create shutdown.js

---
+++ commands/util/shutdown.js
@@ -0,0 +1,30 @@
+const Command = require('../../structures/Command');
+
+module.exports = class ShutdownCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'shutdown',
+			aliases: ['die', 'explode', 'restart', 'process.exit'],
+			group: 'util',
+			memberName: 'shutdown',
+			description: 'Shuts down the bot.',
+			details: 'Only the bot owner(s) may use this command.',
+			guarded: true,
+			ownerOnly: true,
+			args: [
+				{
+					key: 'code',
+					prompt: 'What code do you want to send to `process.exit`?',
+					type: 'integer',
+					default: 0
+				}
+			]
+		});
+	}
+
+	run(msg, { code }) {
+		this.client.logger.info('[SHUTDOWN] Manual shutdown engaged.');
+		process.exit(code);
+		return null;
+	}
+};
