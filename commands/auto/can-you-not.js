ca4b6e8bb347923f29b8ad660e23847e2d742478
JustSomeoneOnGithub <59625582+JustSomeoneOnGithub@users.noreply.github.com>
2020-07-19 15:07:11 -0400

    Create can-you-not.js

---
+++ commands/auto/can-you-not.js
@@ -0,0 +1,19 @@
+const Command = require('../../structures/commands/AutoReply');
+
+module.exports = class CanYouNotCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'can-you-not',
+			aliases: ['can-u-not'],
+			group: 'auto',
+			memberName: 'can-you-not',
+			description: 'Can YOU not?',
+			patterns: [/can (you|u) not/i],
+			reply: true
+		});
+	}
+
+	generateText() {
+		return 'Can YOU not?';
+	}
+};
