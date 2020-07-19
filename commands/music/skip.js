dd294edafb44379410c45cd6ec8d7da1db76c32d
6NCLegend9 <59505853+6NCLegend9@users.noreply.github.com>
2020-07-19 22:30:35 +0300

    Update skip.js

--- commands/music/skip.js
+++ commands/music/skip.js
@@ -1,4 +1,5 @@
 const { Command } = require('discord.js-commando');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = class SkipCommand extends Command {
   constructor(client) {
@@ -24,5 +25,10 @@ module.exports = class SkipCommand extends Command {
       return message.reply(`You can't skip a trivia! tyep *.endtrivia*`);
     }
     message.guild.musicData.songDispatcher.end();
+    const embed = new MessageEmbed()
+      .setColor('#404040')
+     	  	.addField('Skip', `<:green_skip:734454978005041162> Skiped!`)
+   // .addField('Removed', `<:trashcan:713747946608525383> Removed song number ${songNumber} from queue`)
+    	return message.embed(embed);
   }
 };  
