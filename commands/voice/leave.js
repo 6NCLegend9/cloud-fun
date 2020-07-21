1f71f9bd5deff2a1de4193a0c2cd87d2848704d1
6NCLegend9 <59505853+6NCLegend9@users.noreply.github.com>
2020-07-22 02:32:25 +0300

    Update leave.js

--- commands/voice/leave.js
+++ commands/voice/leave.js
@@ -14,13 +14,36 @@ module.exports = class LeaveCommand extends Command {
 
  
  run(message) {
-		const connection = this.client.voice.connections.get(message.guild.id);
+	 const connection = this.client.voice.connections.get(message.guild.id);
 		if (!connection) return message.reply('I am not in a voice channel.');
 		connection.channel.leave();
-//	return message.reply(`Left **${connection.channel.name}**...`);
+  var voiceChannel = message.member.voice.channel;
+    if (!voiceChannel) {
+      message.reply('Join a channel and try again');
+      return;
+    } else if (
+      typeof message.guild.musicData.songDispatcher == 'undefined' ||
+      message.guild.musicData.songDispatcher == null
+    ) {
+      message.reply('There is no song playing right now!');
+      return;
+    } else if (!message.guild.musicData.queue) {
+      message.reply('There are no songs in queue');
+      return;
+    } else if (message.guild.musicData.songDispatcher.paused) {
+      message.guild.musicData.songDispatcher.resume();
+      setTimeout(() => {
+        message.guild.musicData.songDispatcher.end();
+      }, 100);
+      message.guild.musicData.queue.length = 0;
+      return;
+    } else {
+      message.guild.musicData.songDispatcher.end();
+      message.guild.musicData.queue.length = 0;	
      const embed = new MessageEmbed()  
    .setColor(0x00AE86)
     	.addField('Disconnected', `Left **${connection.channel.name}**...`)
     	return message.embed(embed);
 	}
+ }
 };
